8596f82a72094fe4108881b32cc92d024a46dca7
Lorna Kamau <kamaulorna@gmail.com>
2020-06-04 15:01:05 +0300

    create configuration classes

---
+++ app/views.py
@@ -0,0 +1,10 @@
+from flask import render_template
+from app import app
+
+@app.route('/')
+def index():
+    '''
+    View root page function that returns the index page and its data
+    '''
+    title = 'Pitch'
+    return render_template('index.html', title=title)
