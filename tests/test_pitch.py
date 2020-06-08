bf89c3fbb6f7183fc6b91a11ab934c3e46185114
Lorna Kamau <kamaulorna@gmail.com>
2020-06-08 22:55:00 +0300

    add pitch test

---
+++ tests/test_pitch.py
@@ -0,0 +1,43 @@
+import unittest
+from app.models import Pitch, User, Category
+from app import db
+
+class PitchModelTest(unittest.TestCase):
+    def setUp(self):
+
+        self.new_pitch = Pitch(title = "title", pitch_content= "description", upvotes = 1, downvotes = 1, category_id =1, pitcher_id = 1)
+        self.new_user = User(username = "lorna", email ="lorna@gmail.com", bio = "I am cool", profile_pic_path = "image_url", password = 'lorna')
+        self.new_category = Category(category_name = 'category')
+
+        db.session.add(self.new_user)
+        db.session.add(self.new_category)
+        db.session.add(self.new_pitch)
+        db.session.commit()
+
+    def tearDown(self):
+        Pitch.query.delete()
+        User.query.delete()
+        Category.query.delete()
+        db.session.commit()
+
+    def test_save_pitch(self):
+        self.new_pitch.save_pitch()
+        self.assertTrue(len(Pitch.query.all())>0)
+
+    def test_check_instance_variables(self):
+        self.assertEquals(self.new_pitch.title, 'title')
+        self.assertEquals(self.new_pitch.pitch_content, 'description')
+        self.assertEquals(self.new_pitch.upvotes, 1)
+        self.assertEquals(self.new_pitch.downvotes, 1)
+        self.assertEquals(self.new_pitch.pitcher_id,1)
+        self.assertEquals(self.new_pitch.category_id,1)
+
+    def test_get_user_pitch(self):
+        self.new_pitch.save_pitch()
+        get_pitch = Pitch.get_user_pitch(1)
+        self.assertTrue(len(get_pitch) > 0)
+
+    def test_get_category_pitch(self):
+        self.new_pitch.save_pitch()
+        get_pitch = Pitch.get_category_pitch(1)
+        self.assertTrue(len(get_pitch) > 0)
--- tests/test_user.py
+++ tests/test_user.py
@@ -1,16 +1,35 @@
 import unittest
 from app.models import User
+from app import db
 
 class UserModelTest(unittest.TestCase):
     def setUp(self):
-        self.new_user = User(password = 'Mangoes')
+        self.new_user = User(username = "lorna", email ="lorna@gmail.com", bio = "I am cool", profile_pic_path = "image_url", password = 'lorna')
+        db.session.add(self.new_user)
+        db.session.commit()
 
-    def test_password_setter(self): #ascertains that when password is being hashed and the pass_secure contains a value
+    def tearDown(self):
+        User.query.delete()
+        db.session.commit()
+ 
+    def test_password_setter(self):
         self.assertTrue(self.new_user.pass_secure is not None)
 
-    def test_no_access_password(self):
-            with self.assertRaises(AttributeError): #raise attribute error when we try to access the password property
-                self.new_user.password
-
     def test_password_verification(self):
-            self.assertTrue(self.new_user.verify_password('Mangoes'))
+        self.assertTrue(self.new_user.verify_password('diana'))
+
+    def test_save_user(self):
+        self.new_user.save_user()
+        self.assertTrue(len(User.query.all())>0)
+
+    def test_check_instance_variables(self):
+        self.assertEquals(self.new_user.username, 'lorna')
+        self.assertEquals(self.new_user.email, 'lorna@gmail.com')
+        self.assertEquals(self.new_user.bio, 'I am cool')
+        self.assertEquals(self.new_user.profile_pic_path, 'image_url')
+        self.assertTrue(self.new_user.verify_password('lorna'))
+
+
+    def test_no_access_password(self):
+        with self.assertRaises(AttributeError):
+            self.new_user.password 
