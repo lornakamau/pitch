e40936d11ef8a7d2aaf69c4aff3ffbc862d0371d
Lorna Kamau <kamaulorna@gmail.com>
2020-06-08 22:27:02 +0300

    add user test

--- manage.py
+++ manage.py
@@ -12,6 +12,13 @@ manager.add_command('server',Server)
 migrate = Migrate(app,db)
 manager.add_command('db',MigrateCommand)
 
+@manager.command
+def tests():
+    """Run the unit tests."""
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
 @manager.shell #shell is used to test features in our app and for debugging
 def make_shell_context():
     return dict(app = app,db = db,User = User, Comment= Comment, Category=Category, Pitch=Pitch)
